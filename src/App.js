48b4215adc8653687dc71d457e9c6d4dc3c1653c
Daniyal <daniyalwaseem321@hotmail.com>
2020-11-20 15:45:31 +0500

    fourth commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import login from './components/pages/login';
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/login' component={login} />
+          <Redirect to="/" />
         </Switch>
       </Router>
     </>
