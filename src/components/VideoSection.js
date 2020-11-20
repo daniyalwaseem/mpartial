0ffa5666ccbe7d19891ab8ea49bf7aec08813b68
Daniyal <daniyalwaseem321@hotmail.com>
2020-11-21 00:53:01 +0500

    eight commit

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -6,7 +6,7 @@ function VideoSection() {
   return (
     <>
       <div className='video-container'>
-        <video src='https://mpartial-data.s3.us-east-2.amazonaws.com/homepage.mp4' autoPlay loop muted />
+        <video src='https://mpartial-data.s3.us-east-2.amazonaws.com/homepage.mp4' preload="auto" autoPlay loop muted />
         <div className="main_banner_text">
           Impartial, Remote, 3rd-Party,
           <br />
