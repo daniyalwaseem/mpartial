e34baa90e0b7d2120cd80a2d2d95bce62a5d5cc4
Daniyal <daniyalwaseem321@hotmail.com>
2020-11-21 02:11:42 +0500

    video1 type

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -6,7 +6,7 @@ function VideoSection() {
   return (
     <>
       <div className='video-container'>
-        <video src='/videos/video-1.mp4' autoPlay loop muted />
+        <video src='/videos/video-1.mp4' type="video/mp4" autoPlay loop muted />
         <div className="main_banner_text">
           Impartial, Remote, 3rd-Party,
           <br />
